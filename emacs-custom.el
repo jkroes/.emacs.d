09aeccf32297c4e99bb195952bfd6cb4d3dc7e1d
Kroes <Justin.Kroes@cdpr.ca.gov>
2019-10-15 16:53:08 -0700

    Testing package loading

--- emacs-custom.el
+++ emacs-custom.el
@@ -1,14 +1,30 @@
-;; Customization
-;; Ratonale for usng
+;; See:
+;; Customize section of emacs
 ;; https://emacs.stackexchange.com/questions/102/advantages-of-setting-variables-with-setq-instead-of-custom-el
+;; https://stackoverflow.com/questions/22915019/emacs-setq-before-loading
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-global-modes nil)
+ '(confirm-kill-processes nil)
+ '(custom-enabled-themes (quote (dracula)))
+ '(custom-safe-themes
+   (quote
+    ("35b0b0e531731e270708ddb342dc2e576a31fb298dcbc56a206596a43afac54f" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
+ '(delete-by-moving-to-trash t)
+ '(global-display-line-numbers-mode t)
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (company-tabnine 0x0 flx evil-escape evil-collection key-chord ranger pkg aggressive-indent ess-view ess-R-data-view ess which-key use-package quelpa page-break-lines neotree hydra help-fns+ helm-descbinds general evil-tutor dracula-theme doom-themes counsel command-log-mode ace-window))))
+    (company-tabnine 0x0 flx evil-escape ranger pkg aggressive-indent ess-R-data-view ess which-key use-package quelpa page-break-lines hydra help-fns+ helm-descbinds general evil-tutor dracula-theme ffcounsel command-log-mode ace-window)))
+ '(scroll-bar-mode nil)
+ '(scroll-conservatively 1000000 nil nil "Seems to prevent auto-recentering of point when scrolling")
+ '(tool-bar-mode nil)
+ '(winner-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
