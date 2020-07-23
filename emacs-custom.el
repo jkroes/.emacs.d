3829d5dedcf283602d90e8dd6f8b8687f996c29f
Justin Kroes <jkroes14@ucsbalum.com>
2020-07-22 19:03:43 -0700

    Revert font customizations
    To keep font properly set on Windows and MacOS, init.el proved more
    reliable than customization. Fonts appear larger in WSL Emacs than on
    MacOS, so I need to use a smaller font for WSL.

--- emacs-custom.el
+++ emacs-custom.el
@@ -247,7 +247,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "nil" :slant normal :weight normal :height 140 :width normal))))
  '(nobreak-space ((t nil)))
  '(show-paren-match ((t (:background "dark red" :foreground "White" :weight bold))))
  '(whitespace-tab ((t (:foreground "#636363" :inverse-video t)))))
